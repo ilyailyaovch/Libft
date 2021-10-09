d228c41dbb50a053e4783e88030eff7c4df7b97b
ilyailyaovch <64081099+ilyailyaovch@users.noreply.github.com>
2021-10-09 18:52:28 +0300

    Update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -12,9 +12,53 @@
 
 #include "libft.h"
 
-char	*ft_itoa(int n)!!!!!!!!!!!!!!!!!!!!!!!!
+static int	ft_len(int n)
 {
-	char	*str;
+	int	len;
 
+	if (n <= 0)
+		len = 1;
+	else
+		len = 0;
+	while (n != 0)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	char			*str;
+	int			len;
+	int			sign;
+	unsigned int	abs;
 	
+	if (n < 0)
+	{
+		sign *= -1;
+		abs = -n; 
+	}
+	else
+	{
+		sign = 1;
+		abs = n;
+	}
+	len = ft_len(n);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	len = len - 1;
+	if (sign < 0)
+		str[0] = '-';
+	while (abs >= 10)
+	{
+		str[len] = abs % 10 + '0';
+		abs /= 10;
+		len--;
+	}
+	str[len] = abs % 10 + '0';
+	return (str);
 }
